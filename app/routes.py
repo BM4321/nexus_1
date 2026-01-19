aa88bedc61637efd27bd5ae61ee967833383b29a
BM4321 <153984016+BM4321@users.noreply.github.com>
2026-01-19 23:36:00 +0300

    Pagination

--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,37 @@
 from flask import render_template, flash, redirect, url_for, request
 from app import app, routes, models, errors
-from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm
+from app.forms import LoginForm, RegistrationForm, EditProfileForm, EmptyForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
 import sqlalchemy as sa
 from app import db
-from app.models import User
+from app.models import User, Post
 from urllib.parse import urlsplit
 from datetime import datetime, timezone
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    user = {'username': 'Shedrack'}
-    posts =[
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+    posts = [
         {
             'author': {'username': 'John'},
-            'body' : 'beautiful day in Portland!'
+            'body': 'Beautiful day in Portland!'
         },
         {
             'author': {'username': 'Susan'},
-            'body' : 'Demon slayer is the best anime ever!'
+            'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template("index.html", title='Home Page', posts=posts)
+    posts = db.session.scalars(current_user.following_posts()).all()
+    return render_template("index.html", title='Home Page', form=form,
+                           posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -136,3 +144,10 @@ def unfollow(username):
         return redirect(url_for('user', username=username))
     else:
         return redirect(url_for('index'))
+
+@app.route('/explore')
+@login_required
+def explore():
+    query = sa.select(Post).order_by(Post.timestamp.desc())
+    posts = db.session.scalars(query).all()
+    return render_template('index.html', title='Explore', posts=posts)
